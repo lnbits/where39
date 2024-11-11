1052daa9250047313806e43cf1ce8d6d1bc9202d
Arc <ben@arc.wales>
2024-11-11 14:25:45 +0000

    Working

--- __init__.py
+++ __init__.py
@@ -8,8 +8,6 @@ db = Database("ext_where39")
 where39_ext: APIRouter = APIRouter(prefix="/where39", tags=["where39"])
 where39_ext.include_router(where39_generic_router)
 
-where39_ext: APIRouter = APIRouter(prefix="/where39", tags=["Where39"])
-
 where39_static_files = [
     {
         "path": "/where39/static",
